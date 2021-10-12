aaf5f7ed52a2b3eb546648e647f035d278e737d0
Miguel Guevara <jmiguel_guevara@outlook.com>
2021-10-12 14:00:36 -0500

    feat: show photos on carousell gallery

--- src/screens/Photos/index.style.ts
+++ src/screens/Photos/index.style.ts
@@ -1,9 +1,22 @@
 import {StyleSheet} from 'react-native';
+import Color from '../../theme/color';
 
 const styles = StyleSheet.create({
   containerScreen: {
     paddingHorizontal: 10,
     marginTop: 10,
+  },
+  close: {
+    width: 20,
+    height: 20,
+    resizeMode: 'contain',
+    tintColor: Color.white,
+  },
+  closeContainer: {
+    right: 20,
+    top: 40,
+    position: 'absolute',
+    zIndex: 100,
   },
 });
 
--- src/screens/Photos/index.tsx
+++ src/screens/Photos/index.tsx
@@ -1,13 +1,17 @@
-import React, {FC, useContext} from 'react';
-import {FlatList, View} from 'react-native';
+import React, {FC, useContext, useState} from 'react';
+import {FlatList, Image, TouchableOpacity, View} from 'react-native';
 import PhotoCard, {PhotoCardProps} from '../../components/PhotoCard';
 import {PhotosResponse} from '../../services/useGetAlbums';
 import {Context} from '../../store';
 import generalStyles from '../Utils/generalStyles.style';
 import styles from './index.style';
+import {Modal} from 'react-native';
+import ImageViewer from 'react-native-image-zoom-viewer';
 
 const Photos: FC = (): JSX.Element => {
   const {state} = useContext(Context);
+  const [modalhandler, setModalhandler] = useState<boolean>(false);
+  const [photoIndex, setPhotoIndex] = useState<number>(0);
   const parserPhotos = (photos: PhotosResponse[]): PhotoCardProps[] =>
     photos.map(
       item =>
@@ -23,15 +27,39 @@ const Photos: FC = (): JSX.Element => {
     album && album.photos
       ? parserPhotos(album.photos)
       : ([] as PhotoCardProps[]);
+  const photos =
+    album && album.photos ? album.photos.map(item => ({url: item.url})) : [];
+
+  const handleOpenGallery = (index: number): void => {
+    setPhotoIndex(index);
+    setModalhandler(true);
+  };
 
   return (
     <View style={generalStyles.mainContainer}>
       <FlatList
         data={data}
         keyExtractor={item => `${item.id}-photo`}
-        renderItem={({item}) => <PhotoCard {...item} onPress={() => {}} />}
+        renderItem={({item, index}) => (
+          <PhotoCard {...item} onPress={() => handleOpenGallery(index)} />
+        )}
         style={styles.containerScreen}
       />
+      <Modal visible={modalhandler} transparent={true}>
+        <TouchableOpacity
+          style={styles.closeContainer}
+          onPress={() => setModalhandler(false)}>
+          <Image
+            style={styles.close}
+            source={require('../../assets/close.png')}
+          />
+        </TouchableOpacity>
+        <ImageViewer
+          index={photoIndex}
+          onCancel={() => setModalhandler(false)}
+          imageUrls={photos}
+        />
+      </Modal>
     </View>
   );
 };
